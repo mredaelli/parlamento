c19c330730dba3595d38b7d549fcfbb89046f03f
Massimo Redaelli <m.redaelli@gmail.com>
2017-04-18 23:57:05 +0200

    db

--- build.sbt
+++ build.sbt
@@ -6,5 +6,8 @@ scalaVersion := "2.12.1"
 
 libraryDependencies ++= Seq(
   "org.http4s" % "http4s-blaze-client_2.12" % "0.17.0-M1",
-  "org.http4s" %% "http4s-scala-xml" % "0.17.0-M1"
+  "org.http4s" %% "http4s-scala-xml" % "0.17.0-M1",
+
+  "org.tpolecat" %% "doobie-core-cats" % "0.4.1",
+  "org.xerial" % "sqlite-jdbc" % "3.16.1"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
-import scala.xml.{Elem}
 
-object aaa {
+import scala.xml.Elem
+
+object Decoders {
   import org.http4s.scalaxml._
   import org.http4s._
-  implicit def videoDec: EntityDecoder[Elem] = EntityDecoder.decodeBy(MediaType.fromKey("application", "sparql-results+xml")) { msg =>
+  implicit def sparqlXMLdecoder: EntityDecoder[Elem] = EntityDecoder.decodeBy(MediaType.fromKey("application", "sparql-results+xml")) { msg =>
     xml.decode(msg.withContentType(Some(MediaType.`application/xml`)), strict = true)
   }
 }
@@ -14,14 +15,16 @@ object Main {
   import org.http4s.client.blaze._
   import org.http4s.Uri
 
-  import aaa._
+  import Decoders._
 
   val senatoEp: Uri = Uri.uri("http://dati.senato.it") / "sparql" +? ("default-graph-uri", Seq("")) +? ("format", Seq("application/sparql-results+xml")) +? ("timeout", Seq("0")) +? ("debug", Seq("on"))
+
+  def queryURL(queryStr: String) = senatoEp +? ("query", Seq(queryStr))
 
   def run(): Unit = {
     val client = PooledHttp1Client()
 
-    val url: Uri = senatoEp +? ("query", Seq("PREFIX osr: <http://dati.senato.it/osr/> select distinct ?id where { ?id a osr:Ddl } limit 100"))
+    val url: Uri = queryURL("PREFIX osr: <http://dati.senato.it/osr/> select distinct ?id where { ?id a osr:Ddl } limit 100")
 
     val task = client.expect[Elem](url)
 
@@ -37,8 +40,27 @@ object Main {
   }
 }
 
+import doobie.imports._
+
+object DB {
+
+  import doobie.util.transactor.DriverManagerTransactor
+
+  val xa = DriverManagerTransactor[IOLite](
+    "org.sqlite.JDBC", "jdbc:sqlite:sample.db", "", ""
+  )
+  def tr[T](cio: ConnectionIO[T]) = cio.transact(xa).unsafePerformIO
+
+}
+
 object App {
+
+
   def main(args: Array[String]): Unit = {
+    val program2 : ConnectionIO[Int] = sql"select 42".query[Int].unique
+    //val t = program2.transact(xa)
+    DB.tr(sql"CREATE TABLE IF NOT EXISTS DDL (id STRING);".update.run)
+    println(DB.tr(program2))
     Main.run()
   }
 }
