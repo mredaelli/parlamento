e28f6a9854e5ab134ca7f476394448c62898346a
Massimo Redaelli <massimo.redaelli@aia.ch>
2017-04-28 17:46:11 +0200

    better error in json decoding

--- src/main/scala/doobieDecoders.scala
+++ src/main/scala/doobieDecoders.scala
@@ -1,8 +1,12 @@
 import cats.data.EitherT
-import io.circe.Decoder
+import io.circe.{Decoder, DecodingFailure}
 import org.http4s._
 import org.http4s.circe._
 import JsonUtils.EncDec._
+import cats.{Applicative, Unapply}
+import fs2.Task
+import io.circe.Decoder.Result
+import org.http4s
 
 object doobieDecoders {
 
@@ -10,18 +14,27 @@ object doobieDecoders {
     jsonOf[Sparql].decode(msg.withContentType(Some(MediaType.`application/json`)), strict = true)
   }
 
+  def fooU[FA](fa: FA)(implicit U: Unapply[Applicative, FA]): U.M[U.A] =
+    U.subst(fa)
+
   implicit def sparqlJSONdecoderDdls[T<:SparqlRes](implicit imp: Decoder[T]): EntityDecoder[Seq[T]] = EntityDecoder.decodeBy(MediaType.fromKey("application", "sparql-results+json")) { msg =>
     import cats.implicits._
 
-    EitherT {
-      msg.as[Sparql].map( sp => {
+
+      val aa = msg.as[Sparql].map(sp => {
         println(sp)
-        sp.value.flatMap( j => {
+        val ll: Seq[Result[T]] = sp.value.map(j => {
           println(imp.decodeJson(j))
-          imp.decodeJson(j).toOption
-        } ).asRight[DecodeFailure]
+          imp.decodeJson(j)
+        })
+        ll.foldRight( Either(io.circe.DecodingFailure, Seq[T]()) ) {
+          case ( e, (ls, rs) ) => e.fold( l => (ls, rs), r => ( ls, r +: rs ) )
+        }
       })
-    }
+        //
+   EitherT {
+     aa
+   }
   }
 
 }
